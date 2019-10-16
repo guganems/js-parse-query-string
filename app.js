91a3a56b93f0c65516d031aec090b4752b107273
guganems <g_nemsveridze@cu.edu.ge>
2019-10-16 15:06:10 +0400

    add array

--- app.js
+++ app.js
@@ -11,10 +11,28 @@ class Url {
     }
 
     searchStrToObj(searchStr) {
+        let prev = [];
         let searchArr = searchStr.substr(1).split("&");
         for (let each of searchArr) {
             let pair = each.split("=");
-            this[pair[0]] = pair[1];
+            if (prev.includes(pair[0])) {
+                let temp = this[pair[0]];
+                this[pair[0]] = [];
+                if (Array.isArray(temp)) {
+                    for (let eachTemp of temp) {
+                        this[pair[0]].push(eachTemp);
+                    }
+                    this[pair[0]].push(pair[1]);
+                }
+                else {
+                    this[pair[0]].push(temp);
+                    this[pair[0]].push(pair[1]);
+                }
+            }
+            else {
+                this[pair[0]] = pair[1];
+                prev.push(pair[0]);
+            }
         }
     }
 }
